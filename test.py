49551f951db6a37a8efd4084a7666c74bd6d98f4
Pikachú <bethanycheum@microsoft.com>
2025-05-09 17:38:46 +0000

    fix: Ensure APP_PRIVATE_KEY is explicitly converted to string when provided as an argument

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ APP_PRIVATE_KEY = None
 # get private key from args
 
 if len(sys.argv) > 1:
-    APP_PRIVATE_KEY = sys.argv[1]
+    APP_PRIVATE_KEY = str(sys.argv[1])
 
 if APP_PRIVATE_KEY is None:
     print("Please provide the path to the private key file as an argument.")
